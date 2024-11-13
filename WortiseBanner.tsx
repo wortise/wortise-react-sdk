9e8625bdbeb85d728bdf748501a07daeab992602
Miguel Boton <mboton@wortise.com>
2024-11-13 12:40:17 +0100

    Updated to version 1.6.1

--- WortiseBanner.tsx
+++ WortiseBanner.tsx
@@ -12,6 +12,26 @@ type WortiseBannerState = {
 type WortiseBannerView = React.Component<WortiseBannerProps> & NativeMethods;
 
 class WortiseBanner extends React.Component<WortiseBannerProps, WortiseBannerState> {
+  static get AUTO_REFRESH_DEFAULT_TIME() {
+    return 60 * 1000;
+  }
+
+  static get AUTO_REFRESH_DISABLED() {
+    return -1;
+  }
+
+  static get AUTO_REFRESH_MAX_TIME() {
+    return 120 * 1000;
+  }
+
+  static get AUTO_REFRESH_MIN_TIME() {
+    return 30 * 1000;
+  }
+
+  static get AUTO_REFRESH_UNSPECIFIED() {
+    return 0;
+  }
+
   ref = createRef<WortiseBannerView>();
 
   constructor(props: WortiseBannerProps) {
